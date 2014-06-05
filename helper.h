807f025f5ef76830f38867f0ef5d71b03fa5f4b4
Meet Bhagdev <meetbhagdev@ucla.edu>
2014-06-05 06:34:16 +0000

    helper stuff done

--- helper.h
+++ helper.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <signal.h>
-#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
-#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
-#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <sys/types.h>   
+#include <sys/socket.h>  
+#include <netinet/in.h>  
 #include <stdlib.h>
 #include <strings.h>
 #include <sys/time.h>
@@ -16,32 +16,14 @@
 void customBzero(struct packet * pkt)
 {
   
-    pkt->ack_no=0;
+  pkt->ack_no=0;
   pkt->seq_no=0;
   pkt->fin =0;
   pkt->size=0;
   pkt->content_len= 0;
-    memset(pkt->data,0,1004);
+  memset(pkt->data,0,1004);
 }
 int prob( int a ) {
   return ((rand () % 100) < a) ? 1 : 0;
 }
 
-/*Taken from Kirby for now, we can figure out how to do our own timer alter */
-//signal timeout stuff
-volatile sig_atomic_t timeout = 0;
-
-
-
-unsigned int setTimeout(unsigned int microseconds) {
-    timeout = 0;
-    struct itimerval old, current;
-    current.it_interval.tv_usec = 0;
-    current.it_interval.tv_sec = 0;
-    current.it_value.tv_usec = (long int) microseconds; 
-    current.it_value.tv_sec = 0;
-    if (setitimer (ITIMER_REAL, &current, &old) < 0)
-        return 0;
-    else
-        return old.it_value.tv_sec;
-}
